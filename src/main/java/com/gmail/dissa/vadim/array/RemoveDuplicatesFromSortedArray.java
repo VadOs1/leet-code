ca5b4b151a1c8c01ca67e9bd35b1e6595374a744
Vadim Dissa <vadim.dissa@gmail.com>
2018-03-23 11:00:12 +0200

    Add implementation of Remove Duplicates from Sorted Array

--- src/main/java/com/gmail/dissa/vadim/array/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/gmail/dissa/vadim/array/RemoveDuplicatesFromSortedArray.java
@@ -1,4 +1,16 @@
 package com.gmail.dissa.vadim.array;
 
 public class RemoveDuplicatesFromSortedArray {
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[++length] = nums[i];
+            }
+        }
+        return length + 1;
+    }
 }
--- src/test/java/com/gmail/dissa/vadim/array/RemoveDuplicatesFromSortedArrayTest.java
+++ src/test/java/com/gmail/dissa/vadim/array/RemoveDuplicatesFromSortedArrayTest.java
@@ -1,4 +1,20 @@
 package com.gmail.dissa.vadim.array;
 
-public class RemoveDuplicatesFromSortedArrayTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RemoveDuplicatesFromSortedArrayTest {
+    @Test
+    void testDuplicatesRemoval() {
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(null), 0);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{}), 0);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{1}), 1);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 1}), 1);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 2}), 2);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 2, 3}), 3);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 2, 2}), 2);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 2, 3, 4}), 4);
+        assertEquals(RemoveDuplicatesFromSortedArray.removeDuplicates(new int[]{1, 1, 2, 2, 3, 4, 5, 5, 6}), 6);
+    }
 }
