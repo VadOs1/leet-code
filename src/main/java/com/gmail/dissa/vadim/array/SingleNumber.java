15370665c092792acc41141c4afe33449d86870b
Vadim Dissa <vadim.dissa@gmail.com>
2018-03-23 13:53:28 +0200

    Optimize Single Number solution

--- src/main/java/com/gmail/dissa/vadim/array/SingleNumber.java
+++ src/main/java/com/gmail/dissa/vadim/array/SingleNumber.java
@@ -1,11 +1,10 @@
 package com.gmail.dissa.vadim.array;
 
+import java.util.Arrays;
+
 public class SingleNumber {
     public static int singleNumber(int[] nums) {
-        int res = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            res = res ^ nums[i];
-        }
-        return res;
+        return Arrays.stream(nums).reduce((n1, n2) -> n1 ^ n2)
+                .orElseThrow(() -> new IllegalArgumentException("Illegal Argument"));
     }
 }
