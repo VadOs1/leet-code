02329d41b1c6ef4b45558d31db911179265ee43e
Vadim Dissa <vadim.dissa@gmail.com>
2018-03-23 11:09:29 +0200

    Add comment to Remove Duplicates from Sorted Array

--- src/main/java/com/gmail/dissa/vadim/array/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/com/gmail/dissa/vadim/array/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,10 @@
 package com.gmail.dissa.vadim.array;
 
+/**
+ * Given nums=[1,1,2],
+ * Your function should return length=2,with the first two elements of nums being 1 and 2 respectively.
+ * It doesn't matter what you leave beyond the new length.
+ */
 public class RemoveDuplicatesFromSortedArray {
     public static int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) {
