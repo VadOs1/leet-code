dcf5d604d80729a2b77a0b7d50213b5a210079a5
Vadim Dissa <vadim.dissa@gmail.com>
2018-03-26 13:34:20 +0300

    Add task description for SingleNumber

--- src/main/java/com/gmail/dissa/vadim/array/SingleNumber.java
+++ src/main/java/com/gmail/dissa/vadim/array/SingleNumber.java
@@ -2,6 +2,11 @@ package com.gmail.dissa.vadim.array;
 
 import java.util.Arrays;
 
+/**
+ * Given an array of integers, every element appears twice except for one. Find that single one.
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ */
 public class SingleNumber {
     public static int singleNumber(int[] nums) {
         return Arrays.stream(nums).reduce((n1, n2) -> n1 ^ n2)
