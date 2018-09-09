d7cc36986c9651ec8a2b067d7b5efccf10bae1cd
Vadim Dissa <vadim.dissa@gmail.com>
2018-09-09 12:59:51 -0400

    Add missing file

---
+++ src/main/java/com/gmail/dissa/vadim/array/TwoSum.java
@@ -0,0 +1,26 @@
+package com.gmail.dissa.vadim.array;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class TwoSum {
+    static int[] twoSum(int[] nums, int target) {
+        if (nums == null) {
+            return new int[]{0, 0};
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int k = i + 1; k < nums.length; k++) {
+                if (nums[i] + nums[k] == target) {
+                    return new int[]{i, k};
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
